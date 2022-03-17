ea690fc8c4de5a3aa67090001bb5b992d4d43469
Oliver <2in1couple@gmail.com>
2022-03-17 17:38:04 +0200

    Seesam

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+function show(val){
+
+    if(val==1) 
+    {
+        document.getElementById('show').style.display='block';
+    }
+    if(val==2) 
+    {
+        document.getElementById('show').style.display='none';
+    }
+}
